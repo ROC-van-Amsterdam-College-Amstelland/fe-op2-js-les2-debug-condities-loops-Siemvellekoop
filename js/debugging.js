43f3c56124bfdbc35c524dce523b48d95a4c8e85
Siemvellekoop <Siemvellekoop@hotmail.nl>
2019-11-18 14:16:21 +0100

    opdracht1

--- js/debugging.js
+++ js/debugging.js
@@ -8,24 +8,49 @@ function getAge()
 {
 var myAge=document.getElementById("myAge").value; //de eerste variabele is voor je aangemaakt...
 
-    if() //vul de juiste voorwaarde in!
+if(myAge < 10) //vul de juiste voorwaarde in!
         {
-             //kies code hieronder
+             loadChildTheme() ;
+        }
+    else if (myAge < 12)
+        {
+             manTheme() ;
         }
     else
-        {
-             //kies code hieronder
-        }
+    {
+        loadTheme() ;
+    }
 }
+
 
 function loadChildTheme()
 {
- //kies code hieronder
+    document.getElementById("brand").src = "images/logo-01.png" ;
+    document.getElementsByTagName("body")[0].style.backgroundColor = "yellowgreen" ;
+    document.getElementsByTagName("header")[0].style.backgroundColor = "#00AEEF" ;
+    document.getElementsByClassName("active")[0].style.backgroundColor = "green" ;
+    document.getElementById("main-nav").style.backgroundColor = "#00008B";
+    document.getElementById("main-content").style.borderColor = "#00AEEF";
 }
 
 function loadTheme()
 {
- //kies code hieronder
+    document.getElementById("brand").src = "images/logo-02.png" ;
+    document.getElementsByTagName("body")[0].style.backgroundColor = "darkgrey" ;
+    document.getElementsByClassName("active")[0].style.backgroundColor = "#666666" ;
+    document.getElementById("main-nav").style.backgroundColor = "black";
+    document.getElementById("main-content").style.borderColor = "black";
+    document.getElementsByTagName("header")[0].style.backgroundColor = "#CCCCCC" ;
+}
+
+function manTheme()
+{
+    document.getElementById("brand").src = "images/logo-02.png" ;
+    document.getElementsByTagName("body")[0].style.backgroundColor = "red" ;
+    document.getElementsByClassName("active")[0].style.backgroundColor = "black" ;
+    document.getElementById("main-nav").style.backgroundColor = "purple";
+    document.getElementById("main-content").style.borderColor = "yellow";
+    document.getElementsByTagName("header")[0].style.backgroundColor = "blue" ;
 }
 
 //dit is de code waar je uit kunt kiezen om de website goed te laten werken, let op je moet nog wel e.a. toevoegen!
